74a496153d19a7f78be3e9f70fcbbc75b53815a5
Vlad M <vlad.milytin@gmail.com>
2024-02-20 21:53:50 +0100

    Improve with limiting number of tries

--- guess-a-number/src/main.rs
+++ guess-a-number/src/main.rs
@@ -2,14 +2,21 @@ use std::{io, cmp::Ordering};
 use rand::Rng;
 
 fn main() {
+    println!("Enter a number of tries:");
+    let mut num_of_tries = String::new();
+    io::stdin()
+        .read_line(&mut num_of_tries)
+        .expect("Failed to read a line");
+    let mut num_of_tries: u32 = num_of_tries.trim().parse().expect("Expected unsigned int");
+    println!("You set {} number of tries", num_of_tries);
     println!("Guess a number btw 0 and 100");
 
     let secret = rand::thread_rng().gen_range(0..=100);
+    let mut guessed: bool = false;
 
-    println!("Please enter your guess:");
-
-    loop {
+    for i in 0 .. num_of_tries {
         let mut guess = String::new();
+        println!("Please enter your guess:");
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read a line");
@@ -19,10 +26,17 @@ fn main() {
             Ordering::Less => println!("Think greater"),
             Ordering::Greater => println!("Think less"),
             Ordering::Equal => {
+                guessed = true;
                 println!("You are god damn right!");
                 break;
             }
         }
+    }
+
+    if (guessed) {
+        println!("Congrats!!!!");
+    } else {
+        println!("Ooops, number of tries is reached :(");
     }
 
 }
