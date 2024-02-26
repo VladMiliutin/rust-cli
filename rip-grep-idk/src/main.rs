3d2105612d75e6f00573bacd230d25b92c1ec997
Vlad M <vlad.milytin@gmail.com>
2024-02-26 21:04:02 +0100

    Trying to read dir

--- rip-grep-idk/src/main.rs
+++ rip-grep-idk/src/main.rs
@@ -1,4 +1,4 @@
-use std::{path::PathBuf}; 
+use std::{path::PathBuf, error::Error, fs::ReadDir}; 
 
 struct Cli {
     pattern: String,
@@ -10,19 +10,34 @@ fn main() {
     let mut args = std::env::args();
     let pattern = args.nth(args.len() - 2).expect("Missing: `pattern`");
     let path = args.last().expect("Missing: `path`");
-    let path_buff = parsePath(&path);
+    let path_buff = parse_path(&path);
 
-    let cli_opts = Cli {
+    let args = Cli {
         pattern: pattern,
         path: path_buff,
         options: read_options(),
     };
-
-    println!("Pattern: {}, path: {}", cli_opts.pattern, &path);
-    for cli_opt in cli_opts.options {
-       print!("{} \t", cli_opt);
-    }
+    
+    let content: Result<String, std::io::Error> = std::fs::read_to_string(&args.path);
 }
+
+fn find_matches(path: &PathBuf, pattern: &String) -> Result<u16, std::io::Error> {
+    let dir: Result<ReadDir, std::io::Error> = std::fs::read_dir(path);
+    Ok(1)
+}
+
+fn find_matches_in_file(content: &String, pattern: &String) -> Vec<String> {
+    let mut matches: Vec<String> = Vec::new();
+
+    for line in content.lines() {
+       if (line.contains(pattern)) {
+           matches.push(line.to_string());
+       }
+    }
+
+    return matches;
+}
+
 fn read_options() -> Vec<String> {
     let mut options = Vec::new();
     for arg in std::env::args().skip(2) {
@@ -31,6 +46,7 @@ fn read_options() -> Vec<String> {
 
     return options;
 }
-fn parsePath(path: &String) -> PathBuf {
+
+fn parse_path(path: &String) -> PathBuf {
     PathBuf::from(path)
 }
